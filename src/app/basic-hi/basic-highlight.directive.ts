b8f6c2368ca9f11ca4187fe835ffd67912eb7274
Karthik Nair <herbolax77@gmail.com>
2023-08-18 17:13:46 +0530

    first commit

---
+++ src/app/basic-hi/basic-highlight.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef, OnInit } from "@angular/core";
+
+
+@Directive({
+    selector: '[appBasicHighlight]'
+})
+export class BasicHighlightDirective implements OnInit{
+    
+    constructor(private element: ElementRef) {
+
+    }
+    ngOnInit(): void {
+        this.element.nativeElement.style.backgroundColor = 'green';
+    }
+
+}
